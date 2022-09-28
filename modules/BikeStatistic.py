f216cd460b942a686031ef8638d0eb65d6a95819
Miroslaw Lazarewicz <miroslaw.lazarewicz@gmail.com>
2022-09-28 22:05:56 +0200

    BikeStatistic module cleanup

--- modules/BikeStatistic.py
+++ modules/BikeStatistic.py
@@ -8,13 +8,13 @@ from Utils import send_get_request
 
 
 @dataclass
-class Stats:
+class RideStatistic:
     ride_count: int
     summary_distance: int  # kilometers
     elapsed_time: int  # minutes
 
     def __str__(self) -> str:
-        return f"Rides count: {self.ride_count}\nSummary distance: {self.summary_distance} kilometers.\nElapsed time: {self.elapsed_time} minutes."
+        return f'Rides count: {self.ride_count}\nSummary distance: {self.summary_distance} kilometers.\nElapsed time: {self.elapsed_time} minutes. '
 
 
 @dataclass
@@ -24,18 +24,18 @@ class BikeStatistics:
     def __str__(self):
         return self.__dict__
 
-    def get_athlete_statistics(self, athlete_id):
+    #TODO: Return object similar to Stats
+    def get_total_athlete_statistics(self, athlete_id):
         send_get_request(f'https://www.strava.com/api/v3//athletes/{athlete_id}/stats', bearer_token=self.strava_token)
 
-    def get_statistic_between_range(self, before_date, after_date) -> Stats:
+    def get_statistic_between_range(self, before_date, after_date) -> RideStatistic:
         data = send_get_request(
             f'https://www.strava.com/api/v3/athlete/activities?before={before_date}&after={after_date}&per_page=50',
             bearer_token=self.strava_token)
         ride_count = len(data)
         summary_distance = sum([int(single_ride['distance'] * 0.001) for single_ride in data])
         elapsed_time = sum([int(single_ride['elapsed_time']) // 60 for single_ride in data])
-        # TODO: Remove Mock stats
-        stats = Stats(ride_count, summary_distance, elapsed_time)
+        stats = RideStatistic(ride_count, summary_distance, elapsed_time)
         return stats
 
     # TODO: Remove redundant method - this could be simplified
